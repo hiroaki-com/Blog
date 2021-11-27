da9ff825489f7d2ed03d7e1f1e6f1512e7df720c
hiroaki-com <comukichi@gmail.com>
2021-11-27 17:39:59 +0900

    gest-login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,12 @@ class User < ApplicationRecord
 
   #　以下、 allow_nil: true の記述により、名前登録機能の実装前に作成したユーザでのログイン時のエラーを回避することに成功した。
   delegate :name, to: :profile, allow_nil: true
-
+  
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
    :sessions => 'users/sessions'
 }
 
+devise_scope :user do
+  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+end
+
 
   resources :articles
   root "articles#index"
